b72d094ae938904e6fce8a4eb36dbc87a151b6be
Unknown <xvillanueva@mail.csuchico.edu>
2017-04-25 23:30:22 -0800

    part one submission: chico array

--- gallery.js
+++ gallery.js
@@ -1,6 +1,11 @@
-
-
+var chico = ["Hulas","the WERC","In-N-Out","madison bear","burgers & brew"];
+var current
 function shuffleGallery()
 {
-
+var Random = chico[Math.floor(Math.random() * chico.length-1)];
+console.log(chico);
+while (chico < 3) {
+  chico.length += "  " + chico.length;
+  chico.length++;
+}
 }
