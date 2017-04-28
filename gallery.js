88ba39cb8a4bfa4548f74c319ac67c093015f124
Unknown <xvillanueva@mail.csuchico.edu>
2017-04-27 23:34:36 -0800

    gallery part 2

--- gallery.js
+++ gallery.js
@@ -1,11 +1,39 @@
-var chico = ["Hulas","the WERC","In-N-Out","madison bear","burgers & brew"];
-var current
+var chico ={}
+  {
+    description:"Hulas",
+    picture: "https://s-media-cache-ak0.pinimg.com/564x/60/6b/d4/606bd4510123bde9ae79453f2c528bc5.jpg",
+    from:"pinterest.com"
+  },
+  {
+    description:"MOMS",
+    picture: "https://www.csuchico.edu/chicoexperience/_assets/images/moms.jpg",
+    from:"csuchico.edu"
+  },
+  {
+    description:"Madison bear",
+    picture: "https://pbs.twimg.com/profile_images/197076615/bear.jpg",
+    from:"twitter.com"
+  },
+  {
+    description:"IN-N-OUT",
+    picture: "https://s3-media1.fl.yelpcdn.com/bphoto/YaZEFzUDt00-sGNTbrbOBw/o.jpg",
+    from:"yelp.com"
+  },
+  {
+    description:"Burgers and Brew",
+    picture: "https://static.wixstatic.com/media/ebd312_cc84e5720763419aab5d39f21ad39f0b~mv2.png",
+    from:"burgersandbrew.com"
+  };
+
+var current = chico
 function shuffleGallery()
 {
-var Random = chico[Math.floor(Math.random() * chico.length-1)];
-console.log(chico);
-while (chico < 3) {
-  chico.length += "  " + chico.length;
-  chico.length++;
-}
+do{
+  var RandomNumbers = Math.floor(Math.random() * chico.length);
+  console.log(RandomNumbers);
+
+} while(current==RandomNumbers);
+
+
+
 }
